ff5743c9907b42ced6dc169861099c29c9577544
Nick Psaras <Nicholas.Psaras1@marist.edu>
2021-09-28 23:17:32 -0400

    module 2 assignment.

---
+++ area.py
@@ -0,0 +1,13 @@
+import math
+
+def main():
+    print("This program calculates the cost per square inch of a circular object.")
+
+    diameter = eval(input("What is the diameter of the object? "))
+    price = eval(input("What is the price of the whole object? "))
+
+    cost_per_square = (math.pi * (diameter / 2)**2) / price
+
+    print("The cost per square inch is $", round(cost_per_square, 2), sep="")
+
+main()
