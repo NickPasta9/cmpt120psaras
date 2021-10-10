9e45e629de8646f17b0ecac98fb0f176fa850ac2
Juan Arias <juan.arias@marist.edu>
2021-10-10 10:33:32 -0400

    Feedback

--- area.py
+++ area.py
@@ -6,7 +6,7 @@ def main():
     diameter = eval(input("What is the diameter of the object? "))
     price = eval(input("What is the price of the whole object? "))
 
-    cost_per_square = (math.pi * (diameter / 2)**2) / price
+    cost_per_square = price / (math.pi * (diameter / 2)**2)
 
     print("The cost per square inch is $", round(cost_per_square, 2), sep="")
 
