79023a20963e1062f06af1dee1e6ffc911341294
Juan Arias <juan.arias@marist.edu>
2021-11-08 15:31:50 -0500

    Feedback

--- craps1.py
+++ craps1.py
@@ -6,7 +6,7 @@ from random import randrange
 def main():
     printIntro()
     n = eval (input("How many rolls should I simulate? >>"))
-    wins, losses = SimNCraps(n)
+    wins, losses = sim_N_Amount_Craps(n)
     printSummary(wins, losses, n)
 
 def printIntro():
@@ -18,14 +18,14 @@ def sim_N_Amount_Craps(n):
     wins = 0
     losses = 0
     for i in range(n):
-        if simOneGame() is True:
+        if simOneGame(): # JA
             wins = wins + 1
         else:
             losses = losses + 1
         return wins, losses
 
 def simOneGame():
-    x = randrange(2,12)
+    x = randrange(2,13)
     if x == 2 or x == 3 or x == 12:
         return False
     elif x == 7 or x == 11:
@@ -33,7 +33,7 @@ def simOneGame():
     else:
         y = 0
         while x != y and x != 7:
-            y = randrange(2,12)
+            y = randrange(2,13)
             if y == 7:
                 return False
             elif y == x:
@@ -44,6 +44,6 @@ def printSummary(wins, losses, n):
     print("Winning rolls: {0} ({1:0.1%})".format(wins, wins/n))
     print("Losing rolls: {0} ({1:0.1})".format(losses, losses/n))
 
-if __name__ == ' __main__': main()
+if __name__ == '__main__': main()
 
 
--- five_side_dice.py
+++ five_side_dice.py
@@ -7,13 +7,13 @@ def probability_five_of_a_kind(num_trials):
    sums = 0
    
    for _ in range(num_trials):
-       roll_1 = random.randrange(1,6, 1)
-       roll_2 = random.randrange(1,6, 1)
-       roll_3 = random.randrange(1,6, 1)
-       roll_4 = random.randrange(1,6, 1)
-       roll_5 = random.randrange(1,6, 1)
+       roll_1 = random.randrange(1,7, 1)
+       roll_2 = random.randrange(1,7, 1)
+       roll_3 = random.randrange(1,7, 1)
+       roll_4 = random.randrange(1,7, 1)
+       roll_5 = random.randrange(1,7, 1)
        collection = roll_1 + roll_2 + roll_3 + roll_4 + roll_5
-       if collection == 5:
+       if collection == 5: # JA: This does not check for the correct condition
            sums += 1
    prob = round(sums/7776, 8)
    print(f"The probability_five_of_a_kind is {prob}")
